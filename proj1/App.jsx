84365389a1a042380ae5489c0747a21041d23430
Rao Bayyana <nbayyana@bitbucket.org>
2016-06-23 17:28:02 -0700

    Sample react project

---
+++ proj1/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Button from 'react-bootstrap/lib/Button'
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {};
+		this.state.name="rao"
+	}
+   render() {
+      return (
+         <div>
+		<Child name={this.state.name} />
+         </div>
+      );
+   }
+}
+
+class Child extends React.Component {
+   render() {
+	return (
+		<div>
+			<Button bsStyle="success">Default</Button>
+		name is {this.props.name}
+		</div>
+	)
+   }
+}
+
+export default App;
