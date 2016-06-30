1fc78a676abd4353ea9ccda94bb92f64a596a065
eklavya1983 <eklavya1983@github.com>
2016-06-30 16:59:49 -0700

    add remove functionality

--- proj1/App.jsx
+++ proj1/App.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import Button from 'react-bootstrap/lib/Button'
+import {connect} from 'react-redux'
+import {TodoWindow, CmdBox} from './components/components.jsx'
+
+
+const DisplayWindowF = (props) => {
+	return (
+		<TodoWindow todos={props.todos}/>
+	)
+}
+
+function mapStateToProps(state) {
+	return { todos: state.todos }
+}
+const DisplayWindow = connect(mapStateToProps)(DisplayWindowF)
+const CmdBoxC = connect()(CmdBox)
+
 
 class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {};
-		this.state.name="rao"
 	}
-   render() {
-      return (
-         <div>
-	 	Hello
-		<Child name={this.state.name} />
-         </div>
-      );
-   }
-}
-
-class Child extends React.Component {
-   render() {
-	return (
-		<div>
-			<Button bsStyle="success">Default</Button>
-		name is {this.props.name}
-		</div>
-	)
-   }
+	render() {
+		return (
+			<div>
+				<DisplayWindow/>
+				<CmdBoxC/>
+			</div>
+		);
+	}
 }
 
 export default App;
--- proj1/main.js
+++ proj1/main.js
@@ -1,5 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
+import { createStore } from 'redux'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+
+function addToDo(todos, item) {
+	return [item, ...todos];
+}
+
+function removeToDo(todos, item) {
+	return todos.filter((todo) => { return todo.key != item.key } );
+}
+
+function applyAction(state, action) {
+	switch (action.type) {
+		case 'TODO_ADD':
+			return Object.assign(
+				{},
+				state,
+				{todos: addToDo(state.todos, action.data)});
+		case 'TODO_REMOVE':
+			return Object.assign(
+				{},
+				state,
+				{todos: removeToDo(state.todos, action.data)});
+		default:
+			console.log("unknown type: ${action.type}");
+			return state;
+	}
+}
+
+let initialState = {
+	todos : []
+};
+const store = createStore(applyAction, initialState);
+
+function render() {
+	ReactDOM.render(
+		<Provider store={store}><App></App></Provider>,
+		document.getElementById('app'));
+}
+
+function logState() {
+	console.log("State:");
+	console.log(store.getState());
+}
+
+render();
+store.subscribe(logState);
